954473675e784ec01dde235f26953b306592a9cf
Ina Vasilevskaya <inavasilevskaya@gmail.com>
2015-09-23 21:46:55 +0300

    control: import command introduced, vk spiders only

--- postscraper/models.py
+++
@@ -1,9 +0,0 @@
-class Source(object):
-    def __init__(self, name, type, **kwargs):
-        self.name = name
-        self.type = type
-        for name, value in kwargs.items():
-            setattr(self, name, value)
-
-    def to_json(self):
-        import ipdb; ipdb.set_trace()
